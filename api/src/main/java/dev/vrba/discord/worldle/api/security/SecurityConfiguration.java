7f7f1c21c060d0872343a6aa399e809aaae2e01b
Jiří Vrba <jiri@vrba.dev>
2024-09-26 20:18:32 +0200

    Security configuration, yay!

---
+++ api/src/main/java/dev/vrba/discord/worldle/api/security/SecurityConfiguration.java
@@ -0,0 +1,63 @@
+package dev.vrba.discord.worldle.api.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(final @NonNull HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .httpBasic(it -> it.realmName("Worldle"))
+            .authorizeHttpRequests(requests -> {
+                requests.requestMatchers("/api/bot/**").hasRole("BOT");
+                requests.anyRequest().permitAll();
+            })
+            .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource cors() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(
+        final @NonNull @Value("${bot.auth.username}") String username,
+        final @NonNull @Value("${bot.auth.password}") String password
+    ) {
+        return new InMemoryUserDetailsManager(
+            User.builder()
+                .username(username)
+                .password("{noop}" + password)
+                .roles("BOT")
+                .build()
+        );
+
+    }
+}
