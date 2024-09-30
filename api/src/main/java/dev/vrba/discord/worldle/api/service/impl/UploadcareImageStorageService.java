50cb02ff89c04f6ac25d60411d5792023d032e1f
Jiří Vrba <jiri@vrba.dev>
2024-09-30 20:25:24 +0200

    Use a better way to retrieve the full CDN url

--- api/src/main/java/dev/vrba/discord/worldle/api/service/impl/UploadcareImageStorageService.java
+++ api/src/main/java/dev/vrba/discord/worldle/api/service/impl/UploadcareImageStorageService.java
@@ -26,16 +26,13 @@ public class UploadcareImageStorageService implements ImageStorageService {
     private final Logger logger = LoggerFactory.getLogger(UploadcareImageStorageService.class);
 
     @NonNull
-    private static final String CDN_URL_BASE = "https://www.ucarecdn.com";
-
-    @NonNull
     @Override
     public Optional<String> uploadChallengeImage(@NonNull byte[] imageData, @NonNull LocalDate challengeDate) {
         final String filename = challengeDate.format(DateTimeFormatter.BASIC_ISO_DATE) + ".png";
         final Uploader uploader = new FileUploader(client, imageData, filename);
 
         try {
-            final String url = CDN_URL_BASE + uploader.upload().cdnPath().build();
+            final String url = uploader.upload().getOriginalFileUrl().toString();
 
             logger.info("Uploaded challenge image to the following URL: {}", url);
 
