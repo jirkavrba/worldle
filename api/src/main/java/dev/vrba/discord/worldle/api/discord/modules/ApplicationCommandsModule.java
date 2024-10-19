5764ea0819077181c165df7deb8ca64e8af0ec9e
Jiří Vrba <jiri@vrba.dev>
2024-10-19 14:48:25 +0200

    Implement the /subscribe and /unsubscribe commands

--- api/src/main/java/dev/vrba/discord/worldle/api/discord/modules/ApplicationCommandsModule.java
+++ api/src/main/java/dev/vrba/discord/worldle/api/discord/modules/ApplicationCommandsModule.java
@@ -1,10 +1,14 @@
 package dev.vrba.discord.worldle.api.discord.modules;
 
 import dev.vrba.discord.worldle.api.discord.DiscordBotModule;
+import dev.vrba.discord.worldle.api.service.SubscribedChannelService;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.InteractionApplicationCommandCallbackSpec;
 import discord4j.discordjson.json.ApplicationCommandRequest;
 import discord4j.rest.RestClient;
+import discord4j.rest.util.Color;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
@@ -12,15 +16,29 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class ApplicationCommandsModule implements DiscordBotModule {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(ApplicationCommandsModule.class);
 
+    private final static String SUBSCRIBE_COMMAND = "subscribe";
+
+    private final static String UNSUBSCRIBE_COMMAND = "unsubscribe";
+
+    @NonNull
+    private final SubscribedChannelService service;
+
+    public ApplicationCommandsModule(SubscribedChannelService service) {
+        this.service = Objects.requireNonNull(service);
+    }
+
     @NonNull
     @Override
     public Mono<Void> register(@NonNull GatewayDiscordClient client) {
+        Objects.requireNonNull(client);
+
         return registerSlashCommands(client)
                 .then(registerSlashCommandHandlers(client));
     }
@@ -32,13 +50,13 @@ public class ApplicationCommandsModule implements DiscordBotModule {
         final RestClient rest = client.getRestClient();
         final List<ApplicationCommandRequest> requests = List.of(
                 ApplicationCommandRequest.builder()
-                        .name("register")
-                        .description("Registers the channel to daily Worldle challenges")
+                        .name(SUBSCRIBE_COMMAND)
+                        .description("Subscribes the channel to daily Worldle challenges")
                         .dmPermission(false)
                         .build(),
                 ApplicationCommandRequest.builder()
-                        .name("unregister")
-                        .description("Unregisters the channel from daily Worldle challenges")
+                        .name(UNSUBSCRIBE_COMMAND)
+                        .description("Unsubscribes the channel from daily Worldle challenges")
                         .dmPermission(false)
                         .build()
         );
@@ -57,11 +75,58 @@ public class ApplicationCommandsModule implements DiscordBotModule {
         LOGGER.info("Registering application command handlers");
 
         return client.on(ChatInputInteractionEvent.class)
-                .flatMap(event -> {
-                    event.getInteraction()
-                            .getCommandInteraction();
-                    return Mono.empty();
-                })
+                .flatMap(event ->
+                        switch (event.getCommandName()) {
+                            case SUBSCRIBE_COMMAND -> handleSubscribeCommand(event);
+                            case UNSUBSCRIBE_COMMAND -> handleUnsubscribeCommand(event);
+                            default -> Mono.empty();
+                        }
+                )
                 .then();
+    }
+
+    private Mono<Void> handleSubscribeCommand(final ChatInputInteractionEvent event) {
+        service.subscribe(event.getInteraction().getChannelId().toString());
+
+        return event.reply(
+                InteractionApplicationCommandCallbackSpec.builder()
+                        .addEmbed(
+                                EmbedCreateSpec.builder()
+                                        .color(Color.of(0x57F287))
+                                        .title("Channel subscribed")
+                                        .description(
+                                                """
+                                                        This channel has been subscribed to daily worldle challenges.
+                                                        Challenges are usually posted at midnight CEST.
+                                                        
+                                                        To unsubscribe, use the `/unsubscribe` command.
+                                                        
+                                                        Thank you for playing.
+                                                        """)
+                                        .build()
+                        )
+                        .build()
+        );
+    }
+
+    private Mono<Void> handleUnsubscribeCommand(final ChatInputInteractionEvent event) {
+        service.unsubscribe(event.getInteraction().getChannelId().toString());
+
+        return event.reply(
+                InteractionApplicationCommandCallbackSpec.builder()
+                        .addEmbed(
+                                EmbedCreateSpec.builder()
+                                        .color(Color.of(0xED4245))
+                                        .title("Channel unsubscribed")
+                                        .description(
+                                                """
+                                                        This channel will no longer receive the daily Worldle challenges.
+                                                        
+                                                        You can always subscribe back by using the `/subscribe` command.
+                                                        """)
+                                        .build()
+                        )
+                        .build()
+        );
     }
 }
