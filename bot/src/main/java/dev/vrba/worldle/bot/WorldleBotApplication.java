3849e1822f45c96ee740eea4f328797baf74243c
Jiří Vrba <jiri@vrba.dev>
2024-10-19 09:28:20 +0200

    Epico spring momento

---
+++ bot/src/main/java/dev/vrba/worldle/bot/WorldleBotApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.worldle.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldleBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldleBotApplication.class, args);
+	}
+
+}
